cb78ed2dad9a2d7cd6e64f094f4ef6bb0ffa55a3
Mohd Aanas <131160846+MAanas078@users.noreply.github.com>
2025-07-13 16:55:50 +0530

    Update vite.config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5174 },
-  assetsInclude: ['**/*.JPG'] // Add this line to include .jpg files as assets
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5174
+  },
+  assetsInclude: ['**/*.JPG']
 });

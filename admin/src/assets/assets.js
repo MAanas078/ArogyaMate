116119de6768dd9a29b1d238615238da7c27b242
Mohd Aanas <131160846+MAanas078@users.noreply.github.com>
2025-07-13 21:22:13 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -12,7 +12,7 @@ import appointments_icon from './appointments_icon.svg'
 import earning_icon from './earning_icon.svg'
 import patients_icon from './patients_icon.svg'
 
-const admin = new URL('./admin.JPG', import.meta.url).href;
+const admin = new URL('./practo.png', import.meta.url).href;
 
 export const assets = {
     add_icon,

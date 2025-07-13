f5ee811d30dd724b32ef5658683f82e949d17bf0
Mohd Aanas <131160846+MAanas078@users.noreply.github.com>
2025-07-13 16:59:14 +0530

    Update vite.config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: process.env.PORT || 5174
+    port: process.env.PORT || 5174,
+    allowedHosts: ['arogyamate-admin.onrender.com'] // 👈 Add this line
   },
   assetsInclude: ['**/*.JPG']
 });
